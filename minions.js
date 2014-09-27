6184e54876a09b86cfb93e3717945a3c16ce4f24
Sergey Yeremenko <syeremenko@live.com>
2014-09-27 19:18:10 -0400

    Added styling to the board
    Added some CSS styling with game specs.

--- minions.js
+++ minions.js
@@ -7,12 +7,16 @@
 		x     = 'x',
 		o     = 'o',
 		turnNumber = 1,
+		$turnNumber = $('#turn-number'),
 		whosTurn,
 		$whosTurn = $('#whos-turn'),
 		$clickPosition = $('#click-position'),
 		empty = null;
 
 	var whosTurnIsIt = function () {
+		$turnNumber.html(turnNumber);
+
+		// X goes if turnNumber is ODD, else O goes
 		if (turnNumber % 2) {
 			console.log('turnNumber' + '' , turnNumber , 'ODD');
 			turnNumber ++;
@@ -31,18 +35,29 @@
 		var clickPosition = (this.getAttribute("data-board-position"));
 		$clickPosition.text(clickPosition);
 		if (grid[clickPosition] !== null) {
-			alert('taken!');
+			console.log('taken!');
 			return;
-		} else {
+		} else {	
 			grid[clickPosition] = whosTurn;
+			$(this).text(whosTurn);
+			if (whosTurn == x) {
+				$(this).addClass('x-color');
+			} else if (whosTurn == o) {
+				$(this).addClass('o-color');
+			}
+			console.log(grid);
+			checkForWinner();
+			whosTurnIsIt();
 		}
-		console.log(grid);
-		checkForWinner();
-		whosTurnIsIt();
 	});
 
 	var checkForWinner = function () {
+		var $gameOutcome = $('#game-outcome');
+		if (turnNumber > 9) {
+			$gameOutcome.text('Draw!');
+		} else {
 
+		}
 	};
 
 	var initGame = function () {
